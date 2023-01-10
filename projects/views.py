66c6f3389901f7a3cb44a5be41e4c19f7cfeb2fe
lazarobeas <39280428+lazarobeas@users.noreply.github.com>
2023-01-09 23:06:17 -0600

    modified and fixed CRUD Operations
    still needs styling and themes

--- projects/views.py
+++ projects/views.py
@@ -25,16 +25,22 @@ def createProject(request):
     return render(request, "projects/project_form.html", context)
 
 
-def updateProject(request, pk):
-
-    project = Project.objects.get(id=pk)
-    form = ProjectForm(instance=project)
-
+def updateProject(request, pk): #primary key is to track in the form
+    project = Project.objects.get(id=pk) #queries object to the updateProject, prefills all of the back end data to the form
+    form = ProjectForm(instance=project) #chooses the instace/project that we want to edit in the form
     context = {'form': form}
 
     if request.method == 'POST':
-        form = ProjectForm(request.POST, instance=project)
+        form = ProjectForm(request.POST, instance=project) #shows that the data is going to be sent to the instance and then saves it
         if form.is_valid():
             form.save()
             return redirect('projects')
-    return render(request, "projects/project_form.html", context)
+    return render(request, "projects/project_form.html", context) #rendering template
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    context = {'object': project}
+    return render(request, 'projects/delete_template.html', context)
